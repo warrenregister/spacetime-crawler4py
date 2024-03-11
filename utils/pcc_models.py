03600ab0ac96315377968ec372ccd9262ce32b9d
warrenregister <42283094+warrenregister@users.noreply.github.com>
2024-03-10 17:22:12 -0700

    Remove unnecessary files and comment out irrelevant functions

--- utils/pcc_models.py
+++ utils/pcc_models.py
@@ -1,24 +1,24 @@
 from rtypes import pcc_set, dimension, primarykey
 
 
-@pcc_set
-class Register(object):
-    """
-    A class representing a crawler registration with the load balancer.
+# @pcc_set
+# class Register(object):
+#     """
+#     A class representing a crawler registration with the load balancer.
 
-    Attributes:
-        crawler_id (str): A unique identifier for the crawler.
-        load_balancer (tuple): The address (host and port) of the load balancer.
-        fresh (bool): Indicates if the registration is new.
-        invalid (bool): Indicates if the registration is invalid.
-    """
-    crawler_id = primarykey(str)
-    load_balancer = dimension(tuple)
-    fresh = dimension(bool)
-    invalid = dimension(bool)
+#     Attributes:
+#         crawler_id (str): A unique identifier for the crawler.
+#         load_balancer (tuple): The address (host and port) of the load balancer.
+#         fresh (bool): Indicates if the registration is new.
+#         invalid (bool): Indicates if the registration is invalid.
+#     """
+#     crawler_id = primarykey(str)
+#     load_balancer = dimension(tuple)
+#     fresh = dimension(bool)
+#     invalid = dimension(bool)
 
-    def __init__(self, crawler_id, fresh):
-        self.crawler_id = crawler_id
-        self.load_balancer = tuple()
-        self.fresh = fresh
-        self.invalid = False
+#     def __init__(self, crawler_id, fresh):
+#         self.crawler_id = crawler_id
+#         self.load_balancer = tuple()
+#         self.fresh = fresh
+#         self.invalid = False
--- utils/server_registration.py
+++ utils/server_registration.py
@@ -1,48 +1,48 @@
-import os
-from spacetime import Node
-from utils.pcc_models import Register
+# import os
+# from spacetime import Node
+# from utils.pcc_models import Register
 
-def init(df, user_agent, fresh):
-    """
-    Initialize the cache server connection.
+# def init(df, user_agent, fresh):
+#     """
+#     Initialize the cache server connection.
 
-    Args:
-        df (Dataframe): A Spacetime Dataframe object.
-        user_agent (str): The user agent string for the crawler.
-        fresh (bool): Indicates if the cache server should be started fresh.
+#     Args:
+#         df (Dataframe): A Spacetime Dataframe object.
+#         user_agent (str): The user agent string for the crawler.
+#         fresh (bool): Indicates if the cache server should be started fresh.
 
-    Returns:
-        tuple: The address (host and port) of the cache server.
-    """
+#     Returns:
+#         tuple: The address (host and port) of the cache server.
+#     """
 
-    reg = df.read_one(Register, user_agent)
-    if not reg:
-        reg = Register(user_agent, fresh)
-        df.add_one(Register, reg)
-        df.commit()
-        df.push_await()
-    while not reg.load_balancer:
-        df.pull_await()
-        if reg.invalid:
-            raise RuntimeError("User agent string is not acceptable.")
-        if reg.load_balancer:
-            df.delete_one(Register, reg)
-            df.commit()
-            df.push()
-    return reg.load_balancer
+#     reg = df.read_one(Register, user_agent)
+#     if not reg:
+#         reg = Register(user_agent, fresh)
+#         df.add_one(Register, reg)
+#         df.commit()
+#         df.push_await()
+#     while not reg.load_balancer:
+#         df.pull_await()
+#         if reg.invalid:
+#             raise RuntimeError("User agent string is not acceptable.")
+#         if reg.load_balancer:
+#             df.delete_one(Register, reg)
+#             df.commit()
+#             df.push()
+#     return reg.load_balancer
 
-def get_cache_server(config, restart):
-    """
-    Initialize the Spacetime Node and retrieve the cache server address (host and port) as part of the initialization process.
+# def get_cache_server(config, restart):
+#     """
+#     Initialize the Spacetime Node and retrieve the cache server address (host and port) as part of the initialization process.
 
-    Args:
-        config (Config): A Config object containing the crawler configuration.
-        restart (bool): Indicates if the cache server should be restarted.
+#     Args:
+#         config (Config): A Config object containing the crawler configuration.
+#         restart (bool): Indicates if the cache server should be restarted.
 
-    Returns:
-        tuple: The address (host and port) of the cache server obtained during the Spacetime Node initialization.
-    """
-    init_node = Node(
-        init, Types=[Register], dataframe=(config.host, config.port))
-    return init_node.start(
-        config.user_agent, restart or not os.path.exists(config.save_file))
+#     Returns:
+#         tuple: The address (host and port) of the cache server obtained during the Spacetime Node initialization.
+#     """
+#     init_node = Node(
+#         init, Types=[Register], dataframe=(config.host, config.port))
+#     return init_node.start(
+#         config.user_agent, restart or not os.path.exists(config.save_file))
